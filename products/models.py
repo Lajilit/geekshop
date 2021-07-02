41f52d02fdd7d426c28325926acd0d635c467f6e
Lajilit <lajilit@gmail.com>
2021-07-03 00:09:11 +0300

    alter tables Product and ProductCategory

--- products/models.py
+++ products/models.py
@@ -5,42 +5,53 @@ class ProductCategory(models.Model):
     name = models.CharField(
         max_length=64,
         unique=True,
-        verbose_name='Наименование',
+        verbose_name='наименование',
     )
     description = models.TextField(
         blank=True,
         null=True,
-        verbose_name='Описание',
+        verbose_name='описание',
     )
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
 
 class Product(models.Model):
     name = models.CharField(
         max_length=256,
-        verbose_name='Наименование',
+        verbose_name='наименование',
     )
     image = models.ImageField(
         upload_to='products_images',
         blank=True,
-        verbose_name='Изображение',
+        verbose_name='изображение',
     )
     description = models.CharField(
         max_length=256,
         blank=True,
         null=True,
-        verbose_name='Описание',
+        verbose_name='описание',
     )
     price = models.DecimalField(
         max_digits=8,
         decimal_places=2,
-        verbose_name='Цена',
+        verbose_name='цена',
     )
     quantity = models.PositiveIntegerField(
         default=2,
-        verbose_name='Количество',
+        verbose_name='количество',
     )
     category = models.ForeignKey(
         ProductCategory,
         on_delete=models.CASCADE,
-        verbose_name='Категория',
+        verbose_name='категория',
     )
+
+    class Meta():
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
