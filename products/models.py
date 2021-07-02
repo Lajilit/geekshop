050c193e82f2305c5fb5112ba0ed8faa74911714
Lajilit <lajilit@gmail.com>
2021-07-02 23:41:51 +0300

    created model Product

--- products/models.py
+++ products/models.py
@@ -2,5 +2,45 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=64, unique=True, verbose_name='Наименование')
-    description = models.TextField(blank=True, verbose_name='Описание')
+    name = models.CharField(
+        max_length=64,
+        unique=True,
+        verbose_name='Наименование',
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание',
+    )
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Наименование',
+    )
+    image = models.ImageField(
+        upload_to='products_images',
+        blank=True,
+        verbose_name='Изображение',
+    )
+    description = models.CharField(
+        max_length=256,
+        blank=True,
+        null=True,
+        verbose_name='Описание',
+    )
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        verbose_name='Цена',
+    )
+    quantity = models.PositiveIntegerField(
+        default=2,
+        verbose_name='Количество',
+    )
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.CASCADE,
+        verbose_name='Категория',
+    )
